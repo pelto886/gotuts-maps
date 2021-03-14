fe896f7a2c9960e5d4bef75f4c021ae8aa23987f
pelto886 <jonas.peltomaki@gmail.com>
2021-03-14 10:44:21 +0100

    added comments about map vs struct differences

--- main.go
+++ main.go
@@ -2,6 +2,19 @@ package main
 
 import "fmt"
 
+// Maps and structs differ in some ways:
+// Maps have all keys of same type
+// maps have all values of same type
+// maps keys are indexed we can iterate
+// maps represent a collection of similar properties
+// keys can be added after compile time
+// maps are a reference type (no pointer needed)
+
+// structs can have values of different type
+// structs keys does not support indexing (no iteration)
+// structs need to know all the different fields at compile
+// used to represent an entity with different properties
+// structs are value types, need pointers.
 func main() {
 	var testColors map[string]string
 
